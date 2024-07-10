995c15c0b0cf41f2205334a97753445898c1d483
Agu Orcha <aguorcha@gmail.com>
2024-07-10 18:59:51 +0200

    Traducido casi todo el CSS de los fondos a Tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,20 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="custom-background h-screen">
-    <div class="color-background"></div>
-    <div class="image-background"></div>
-  </div>
+  <div class="h-screen">
+    <div class="color-background absolute top-0 left-0 w-full h-full z-1"></div>
+    <div class="image-background absolute top-0 left-0 w-full h-full bg-[url('src/assets/images/bg-mobile.png')] lg:bg-[url('src/assets/images/bg-desktop.png')] bg-cover lg:bg-contain bg-no-repeat z-2"></div>
+  </div> 
 </template>
 
 <style scoped>
-.background-container {
-  position: relative;
-}
-
-.color-background, .image-background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-
 .color-background {
   background: linear-gradient(to bottom, hsl(229, 57%, 11%) 100%, transparent 50%);
-  z-index: 1;
-}
-
-.image-background {
-  background-image: url('src/assets/images/bg-mobile.png');
-  background-size: cover;
-  background-repeat: no-repeat;
-  z-index: 2;
 }
 
 @media (min-width: 1024px) {
   .image-background {
-    background-image: url('src/assets/images/bg-desktop.png');
     background-position: center calc(100% + 80px);
-    background-size: contain;
   }
 }
 </style>
